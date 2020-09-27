f179c98f53448c37c5651f78b14007290622a256
zidol <ajh3166@naver.com>
2020-09-27 22:20:19 +0900

    jpa relation mapping setting

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); //애플리케이션 처음 실행 될때
+
+        EntityManager em = emf.createEntityManager(); // 사용자 요청시
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
